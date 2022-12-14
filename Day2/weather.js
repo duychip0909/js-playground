346116e55bb5591dae1e55d02cd8242a6e6978fb
duychip9901 <duychip9901@gmail.com>
2022-12-15 01:02:50 +0700

    update

--- Day2/weather.js
+++ Day2/weather.js
@@ -10,42 +10,49 @@ const submitBtn = document.querySelector('button[name="submit"]');
 
 submitBtn.addEventListener('click', function () {
     fetchWeather();
+
 })
 
+function fetchWeather() {
 
-
-
-// function fetchWeather() {
-//     xhrWeather.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+lat.value+"&lon="+lon.value+"&appid=65e6acbd89514013ffb82d788984105c");
+    fetchLocation();
+    xhrWeather.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+lat.value+"&lon="+lon.value+"&appid=65e6acbd89514013ffb82d788984105c");
     
-//     xhrWeather.onreadystatechange = function () {
-//         if (xhrWeather.readyState === 4) {
-//             if (xhrWeather.status === 200) {
-//                 console.log("Request weather API succesfully!!!");
-//                 let listInfo = JSON.parse(xhrWeather.responseText);
-//                 console.log(listInfo)
-//                 let location = `${listInfo.name}`
-//                 let celcius = `${(listInfo.main.feels_like - 273.15).toFixed(1)}`;
-//                 place.innerHTML = `${location}`;
-//                 temp.innerHTML = `${celcius}°C`;
-//             } else {
-//                 console.log("Error");
-//             }
-//         }
-//     }
+    xhrWeather.onreadystatechange = function () {
+        if (xhrWeather.readyState === 4) {
+            if (xhrWeather.status === 200) {
+                console.log("Request weather API succesfully!!!");
+                let listInfo = JSON.parse(xhrWeather.responseText);
+                console.log(listInfo)
+                let location = `${listInfo.name}`
+                let celcius = `${(listInfo.main.feels_like - 273.15).toFixed(1)}`;
+                place.innerHTML = `${location}`;
+                temp.innerHTML = `${celcius}°C`;
+            } else {
+                console.log("Error");
+            }
+        }
+    }
     
-//     xhrWeather.send();
-// }
+    xhrWeather.send();
+}
 
 function fetchLocation() {
-    xhrLocation.open("GET", "http://api.openweathermap.org/geo/1.0/direct?q=VietNam&limit=5&appid=65e6acbd89514013ffb82d788984105c");
+    const city1 = 'Hanoi';
+    const city2 = 'Ho_Chi_Minh';
+    const city3 = 'Canada';
+    xhrLocation.open("GET", "http://api.openweathermap.org/geo/1.0/direct?q="+city1+"&limit=1&appid=65e6acbd89514013ffb82d788984105c");
     
     xhrLocation.onreadystatechange = function () {
         if (xhrLocation.readyState === 4) {
             if (xhrLocation.status === 200) {
                 console.log("Request location API succesfully!!!");
                 let listInfo = JSON.parse(xhrLocation.responseText);
-                console.log(listInfo)
+                for (let i in listInfo) {
+                    let feature = listInfo[i]
+                    // console.log(feature)
+                }
+                getLocation(listInfo);
             } else {
                 console.log("Error");
             }
@@ -55,5 +62,15 @@ function fetchLocation() {
     xhrLocation.send();
 }
 
-fetchLocation();
+function getLocation(list) {
+    return list.map((location) => {
+        location;
+    })
+}
+
+fetchLocation()
+getLocation();
+// console.log(location)
+
+
 
